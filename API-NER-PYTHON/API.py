b625595eef69ef8ec4eba2aac1053426d1dc696a
humbalang22 <humbalang22@gmail.com>
2023-11-07 18:59:09 +0700

    first commit

---
+++ API-NER-PYTHON/API.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from pathlib import Path
+import spacy
+import json
+
+app = Flask(__name__)
+
+@app.route('/hello/<number>', methods=['GET', 'POST'])
+def welcome(number):
+    output_dir=Path("/home/pradeva/Documents/NER-API/MODEL")
+
+    nlp = spacy.load(output_dir)
+
+    doc = nlp(number)
+
+    res={}
+
+    i = 1
+    for ent in doc.ents:
+        temp = {}
+        temp[ent.label_] = ent.text
+        res[i] = temp
+        i = i + 1
+
+    json_string = json.dumps(res)
+    return json_string
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
